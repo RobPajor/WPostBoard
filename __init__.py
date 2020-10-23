1542cbd96bf8a4b61506cac16ec0848de2d61fa1
Robert Pajor <50381813+RobPajor@users.noreply.github.com>
2020-10-23 10:26:37 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    print("\n----- New Instance -----")
+    app = Flask(__name__,instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path,"flaskr.sqlite"),
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+        )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py",silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from flaskr2 import db
+    db.init_app(app)
+    db.init_sql(app)
+
+    from flaskr2 import auth
+    app.register_blueprint(auth.bp)
+    
+
+    from flaskr2 import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint="index")
+    app.jinja_env.globals.update(check_if_liked=blog.check_if_liked)
+    app.jinja_env.globals.update(check_if_disliked=blog.check_if_disliked)
+
+    from flaskr2 import profile
+    app.register_blueprint(profile.bp)
+    app.add_url_rule("/", endpoint="profile")
+
+    return app
+
+
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):         # This can be used as a Config object instead of config using mapping
+    pass
+
